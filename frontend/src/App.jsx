561c07de10e732670628ce5903286f9d42d9c5e9
Phoenix2157 <69739582+rayyan21d@users.noreply.github.com>
2024-03-14 12:44:50 +0530

    Added the "/" path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     <div>
         <BrowserRouter>
           <Routes >
+            <Route path="/" element={<SignUp />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/dashboard" element={<Dashboard />} />
